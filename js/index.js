aee5816e2e7b71f0d052e3e38297013fe616817c
RubiaHermans <147212420+RubiaHermans@users.noreply.github.com>
2024-03-25 13:18:32 -0700

    First changes

---
+++ js/index.js
@@ -0,0 +1,15 @@
+
+
+
+
+console.log("hello there");
+
+$("#viewproject").click(function(){
+   console.log("hello peixe");
+  });
+
+$("#flip").click(function(){
+ console.log("hello boi");
+    $("#panel").slideToggle("slow");
+  });
+
